0a770cc392806515c80d159562fc8dc0492851d7
DhruvPaghdal <dhruvpaghdal2@gmail.com>
2025-11-02 21:32:14 -0500

    Added base template-layout and urls

--- core/views.py
+++ core/views.py
@@ -1,3 +1,32 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'featured_items': ['Featured-Items-1', 'Featured-Items-2'],
+        'featured_tips': ['Featured-Tips-1', 'Featured-Tips-2'],
+        'recent_tips': ['Recent-Tips-1', 'Recent-Tips-2'],
+        'item_categories': 5,
+        'total_items': 25,
+        'total_tips': 20,
+        'total_centers': 10,
+    }
+    return render(request, 'core/home.html', context)
+
+def dashboard(request):    
+    context = {
+        'user_items': ['Items-1', 'Itmes-2'],
+        'user_tips': ['Tips-1', 'Tips-2'],
+        'favorite_tips': ['Favorite-Tips-1', 'Favorite-Tips-2'],
+        'recently_viewed_items': ['Viewed-Items-1', 'Viewed-Items-2'],
+        'recently_viewed_tips': ['Viewed-Tips-1', 'Viewed-Tips-2'],
+        'total_user_items': 15,
+        'total_user_tips': 10,
+        'total_favorites': 6,
+    }
+    return render(request, 'core/dashboard.html', context)
+
+def about(request):
+    return render(request, 'core/about.html')
+
+def contact(request):
+    return render(request, 'core/contact.html')
